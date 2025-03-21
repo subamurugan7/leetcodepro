06c12f2e86c5bccb9035a9847c76e633aca21b12
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-03-21 09:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (23.92%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,8 +1,15 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        for(int i=1;i<arr.length-1;i++){
-            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
-                return i;
+        int s=0,e=arr.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]){
+                return mid;
+            }
+            else if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }else{
+                e=mid-1;
             }
         }
         return -1;
