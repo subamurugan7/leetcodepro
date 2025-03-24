d6a1c5162a1c6d3285e4c576935597dd2a52b28b
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-03-24 15:21:46 +0530

    Time: 2 ms (51.93%) | Memory: 41.4 MB (47.46%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -2,13 +2,16 @@ class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1; i <= nums.length; i++) {
-            int c = 0;
-            for (int j = 0; j < nums.length; j++) {
-
-                if (nums[j] >= i) {
-                    c = c + 1;
+            int s = 0, e = nums.length - 1, c = 0;
+            while (s <= e) {
+                int m = s + (e - s) / 2;
+                if (nums[m] >= i) {
+                    e = m - 1;
+                } else {
+                    s = m + 1;
                 }
             }
+            c = nums.length - s;
             if (c == i) {
                 return i;
             }
@@ -16,3 +19,23 @@ class Solution {
         return -1;
     }
 }
+// class Solution {
+// public int specialArray(int[] nums) {
+// Arrays.sort(nums);
+// for (int i = 1; i <= nums.length; i++) {
+// int c = 0;
+// for (int j = 0; j < nums.length; j++) {
+
+// if (nums[j] >= i) {
+// c = c + 1;
+// }
+// }
+// }
+// if (c == i) {
+// return i;
+// }
+// }
+// return -1;
+// }
+// }
+//
