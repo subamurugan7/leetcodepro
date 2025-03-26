8089cd3ee79332e7a4b36db90ffc573f543f7984
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-03-26 13:00:17 +0530

    Time: 12 ms (73.08%) | Memory: 46.9 MB (96.25%) - LeetSync

--- 436-find-right-interval/find-right-interval.java
+++ 436-find-right-interval/find-right-interval.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
-        int[] result = new int[n];
-
-        int[][] indexedIntervals = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            indexedIntervals[i] = new int[] { intervals[i][0], intervals[i][1], i };
+        int[] ans = new int[n];
+        int[][] indexintervals = new int[n][3];
+        for (int i = 0; i < intervals.length; i++) {
+            indexintervals[i] = new int[] { intervals[i][0], intervals[i][1], i };
         }
-
-        Arrays.sort(indexedIntervals, (a, b) -> Integer.compare(a[0], b[0]));
-
+        Arrays.sort(indexintervals, (a, b) -> Integer.compare(a[0], b[0]));
         for (int i = 0; i < n; i++) {
-            int end = indexedIntervals[i][1];
-            int left = 0, right = n - 1, idx = -1;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (indexedIntervals[mid][0] >= end) {
-                    idx = indexedIntervals[mid][2];
-                    right = mid - 1;
+            int end = intervals[i][1];
+            int l = 0, r = n - 1, index = -1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                if (indexintervals[m][0] >= end) {
+                    index = indexintervals[m][2];
+                    r = m - 1;
                 } else {
-                    left = mid + 1;
+                    l = m + 1;
                 }
             }
-
-            result[indexedIntervals[i][2]] = idx;
+            ans[i] = index;
         }
-
-        return result;
+        return ans;
     }
 }
