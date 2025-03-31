94ddd76cec11ec861b3bb56dfda3d6d55c65b9e3
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-03-31 11:54:21 +0530

    Time: 3 ms (81.02%) | Memory: 45.6 MB (70.81%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-     int n=nums.length;
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1){
+                return new int[]{nums[index],index+1};
+            }
+        }
+        return new int[]{-1,-1};
+   /* int n=nums.length;
      int[] count =new int[n+1];
      int dup=-1,mis=-1;
      for(int num:nums){
@@ -15,5 +31,11 @@ class Solution {
         }
      }
      return new int[]{dup,mis};
+     */
+    }
+    void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 }
