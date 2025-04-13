4d2ff9e96d9b4a3de2eb97a00e72269d87ba1ead
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-04-13 23:45:28 +0530

    Time: 16 ms (96.56%) | Memory: 55.5 MB (91.98%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,18 @@
 class Solution {
     public int[] sortArray(int[] nums) {
      Arrays.sort(nums);
-     return nums;   
+     return nums;
+     
+     /*for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+     }
+     return nums;
+     */   
     }
 }
