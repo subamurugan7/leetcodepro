5f29bc87ce0e43c9469e5caeb04430c24617d0df
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-07-24 13:13:20 +0530

    Time: 7 ms (73.56%) | Memory: 42.9 MB (57.89%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s1==null||s2==null) return false;
         if(s1.length()>s2.length()){
             return false;
         }
