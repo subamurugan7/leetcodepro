cbd42111d1e627a23bf7304e7b128e695c306f3b
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-07-24 13:08:32 +0530

    Time: 8 ms (60.62%) | Memory: 43 MB (50.09%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int[] s1arr=new int[26];
+        int[] s2arr=new int[26];
+        for(int i=0;i<s1.length();i++){
+            s1arr[s1.charAt(i)-'a']++;
+            s2arr[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(s1arr,s2arr)) return true;
+        for(int i=s1.length();i<s2.length();i++){
+             s2arr[s2.charAt(i)-'a']++;
+             s2arr[s2.charAt(i-s1.length())-'a']--;
+             if(Arrays.equals(s1arr,s2arr)) return true;
+        }
+        return false;
+    }
+}
