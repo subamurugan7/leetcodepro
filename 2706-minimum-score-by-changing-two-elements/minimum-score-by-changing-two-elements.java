07d726843c0802726fe365547b5d1b05ade194e9
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-10-13 19:57:16 +0530

    Time: 15 ms (48.63%) | Memory: 55.6 MB (93.84%) - LeetSync

---
+++ 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = Math.min(
+                Math.min(nums[n - 1] - nums[2], nums[n - 3] - nums[0]),
+                nums[n - 2] - nums[1]);
+        return ans;
+
+    }
+}
