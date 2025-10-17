16b0bf810de8dca891fc8b02001755ccc8ed6cc6
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-10-18 00:19:52 +0530

    Time: 4 ms (61.02%) | Memory: 62.3 MB (30.51%) - LeetSync

---
+++ 2604-minimum-operations-to-make-array-equal-ii/minimum-operations-to-make-array-equal-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long minOperations(int[] nums1, int[] nums2, int k) {
+         if (k == 0) {
+            for (int i = 0; i < nums1.length; i++) {
+                if (nums1[i] != nums2[i]) return -1;
+            }
+            return 0;
+        }
+        long pos = 0;
+        long neg = 0;
+        
+        for (int i = 0; i < nums1.length; i++) {
+            long diff = nums2[i] - nums1[i];
+            
+            // Check divisibility
+            if (k != 0 && diff % k != 0) return -1;
+            
+            if (diff > 0) pos += diff;
+            else neg += -diff; // sum of absolute negatives
+        }
+        
+        // Total positives must equal total negatives
+        if (pos != neg) return -1;
+        
+        return pos / k;
+    }
+}
