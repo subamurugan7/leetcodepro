672bc11d8acfc516b764b09181363e18b9ebd9b6
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-10-30 16:12:50 +0530

    Time: 4 ms (99.69%) | Memory: 45.4 MB (81.11%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        return myvalidPalindrome(s,0,s.length()-1,1);
+       return mypalindrome(s,0,s.length()-1,1);
     }
-    private boolean myvalidPalindrome(String s,int start,int end,int delete){
+    private boolean mypalindrome(String s,int start,int end,int count){
         while(start<end){
             if(s.charAt(start)==s.charAt(end)){
                 start++;
                 end--;
             }
             else{
-               if(delete==0) return false;
-               return myvalidPalindrome(s,start+1,end,0)||myvalidPalindrome(s,start,end-1,0);
+               if(count==0) return false;
+               return mypalindrome(s,start+1,end,0) || mypalindrome(s,start,end-1,0) ;
             }
         }
         return true;
