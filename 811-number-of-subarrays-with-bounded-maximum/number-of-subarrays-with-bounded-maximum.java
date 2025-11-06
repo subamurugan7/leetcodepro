8613f5700b8a1e841f9e8509c862b562f23e5f97
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2025-11-06 19:19:09 +0530

    Time: 3 ms (99.82%) | Memory: 59.7 MB (5.25%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -1,21 +1,18 @@
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-       int ind = 0;
-       int start = 0, end = 0, count = 0;
-       while(end < nums.length){
-        if(nums[end] > right){
-            end = end+1;
-            start = end;
-            ind = end;
-        }else{
-            if(nums[end] >= left){
-                ind = end;
+        return count(nums, right) - count(nums, left - 1);
+    }
+
+    private int count(int[] nums, int bound) {
+        int cur = 0, ans = 0;
+        for (int x : nums) {
+            if (x <= bound) {
+                cur++; 
+            } else {
+                cur = 0; 
             }
-            if(nums[ind] >= left)
-            count += ind-start+1;
-            end++;
+            ans += cur;
         }
-       }
-       return count; 
+        return ans;
     }
 }
