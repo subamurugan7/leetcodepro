c797c77a06836c58ca078a4fe4648a6f384e5b0a
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2026-01-01 22:36:01 +0530

    Time: 6 ms (2.65%) | Memory: 46 MB (10.50%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 return true;
