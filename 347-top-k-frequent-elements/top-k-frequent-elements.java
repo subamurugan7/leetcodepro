13216c5b1998407304812088d59f05821eb40be4
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2026-01-05 11:45:38 +0530

    Time: 13 ms (79.04%) | Memory: 47.9 MB (23.31%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,31 +1,40 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        for (int n : nums) {
-            counter.put(n, counter.getOrDefault(n, 0) + 1);
+        // Step 1: Build frequency map
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-        
-        List<Integer>[] freq = new ArrayList[nums.length + 1];
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
+
+        // Step 2: Build reverse map to map frequencies to the list of numbers with that frequency
+        HashMap<Integer, List<Integer>> reverseCountMap = new HashMap<>();
+        for (int num : countMap.keySet()) {
+            int count = countMap.get(num);
+            reverseCountMap.computeIfAbsent(count, x -> new ArrayList<>()).add(num);
         }
-        
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            int frequency = entry.getValue();
-            freq[frequency].add(entry.getKey());
-        }
-        
+
+        // Step 3: Prepare result array
         int[] res = new int[k];
-        int idx = 0;
-        for (int i = freq.length - 1; i >= 0; i--) {
-            for (int num : freq[i]) {
-                res[idx++] = num;
-                if (idx == k) {
-                    return res;
+        int index = 0;
+
+        // Step 4: Loop from the maximum possible frequency down to 1
+        for (int i = nums.length; i >= 1; i--) {
+            if (reverseCountMap.containsKey(i)) {
+                for (int num : reverseCountMap.get(i)) {
+                    if (index < k) {
+                        res[index++] = num;
+                    } else {
+                        break;
+                    }
                 }
             }
+            if (index >= k) {
+                break;
+            }
         }
-        
-        return new int[0];        
+
+        return res;
     }
 }
