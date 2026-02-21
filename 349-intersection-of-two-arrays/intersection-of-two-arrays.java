20f661ac8fc63fa365de4afff20d04e76450e952
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2026-02-21 13:38:25 +0530

    Time: 7 ms (18.09%) | Memory: 45.2 MB (40.80%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set=new HashSet<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i=0,j=0;
-        while(i<nums1.length&&j<nums2.length){
-            if(nums1[i]<nums2[j]){
-                i++;
-            }
-            else if(nums1[i]>nums2[j]){
-                j++;
-            }
-            else{
-                set.add(nums1[i]);
-                i++;
-                j++;
-            }
+      Arrays.sort(nums1);
+      Arrays.sort(nums2);
+      HashSet<Integer> set=new HashSet<>();
+      int i=0;
+      int j=0;
+      while(i<nums1.length&&j<nums2.length){
+        if(nums1[i]==nums2[j]){
+            set.add(nums1[i]);
+            i++;
+            j++;
         }
-        int[] ans=new int[set.size()];
-        int k=0;
-        for(int in:set){
-            ans[k++]=in;
+        else if(nums1[i]>nums2[j]){
+            j++;
         }
-       return ans;
+        else{
+            i++;
+        }
+      }  
+      int[] ans=new int[set.size()];
+      int k=0;
+      for(int num:set){
+        ans[k++]=num;
+      }
+      return ans;
     }
 }
