cc6d2e20b53dfe887dd589513dc4cb36a1ccd9fd
Subalakshmi M <162044227+subamurugan7@users.noreply.github.com>
2026-02-23 23:23:54 +0530

    Time: 1 ms (84.08%) | Memory: 42.9 MB (71.93%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,29 +1,31 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> countStack = new Stack<>();
-        Stack<StringBuilder> stringStack = new Stack<>();
-        StringBuilder current = new StringBuilder();
-        int num = 0;
-
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            } else if (ch == '[') {
-                countStack.push(num);
-                stringStack.push(current);
-                num = 0;
-                current = new StringBuilder();
-            } else if (ch == ']') {
-                int repeat = countStack.pop();
-                StringBuilder prev = stringStack.pop();
-                for (int i = 0; i < repeat; i++) {
-                    prev.append(current);
-                }
-                current = prev;
-            } else {
-                current.append(ch);
-            }
+      Stack<Integer> countstack=new Stack<>();
+      Stack<StringBuilder> stringstack=new Stack<>();
+      StringBuilder curr=new StringBuilder();
+      int num=0;
+      for(char ch:s.toCharArray()){
+        if(Character.isDigit(ch)){
+            num=num*10+(ch-'0');
         }
-        return current.toString();
+        else if(ch=='['){
+            countstack.push(num);
+            stringstack.push(curr);
+            num=0;
+            curr=new StringBuilder();
+        }
+        else if(ch==']'){
+            int rep=countstack.pop();
+            StringBuilder str=stringstack.pop();
+            for(int i=0;i<rep;i++){
+               str.append(curr);
+            }
+            curr=str;
+        }
+        else{
+            curr.append(ch);
+        }
+      }  
+      return curr.toString();
     }
 }
